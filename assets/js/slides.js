8388a418357de956a94930e6d2fb577464b84610
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2022-05-12 16:25:26 -0700

    added stop/start, animated dot colors

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -5,6 +5,7 @@ var slideIndex = 0;
 var slideTimer = 0;
 var slidesDelay = 5000;
 var slidesFinalDelay = 2; // if zero, don't cycle, just hold on end slide
+var slidePause = false;
 showSlides();
 
 function currentSlide(n) {
@@ -26,13 +27,32 @@ function plusSlides(delta) {
   showSlides();
 }
 
-function showSlides(Delay) {
+function stopStartSlides() {
+  slidePause = !slidePause;
+  var playMark = document.getElementById("slide-play");
+  var pauseMark = document.getElementById("slide-paused");
+  if (slidePause) {
+    if (slideTimer) clearTimeout(slideTimer);
+    if (pauseMark) pauseMark.style.display = "block";
+    if (playMark) playMark.style.display = "none";
+  } else {
+    if (pauseMark) pauseMark.style.display = "none";
+    if (playMark) playMark.style.display = "block";
+    showSlides();
+  }
+}
+
+function showSlides(Delay, ForcePlay) {
   if (Delay) slidesDelay = Delay;
+  if (ForcePlay) slidePause = false;
   var adjDelay = slidesDelay;
   var slides = document.getElementsByClassName("mySlides");
   if (slides.length < 1) return;
+  var dots = document.getElementsByClassName("slide-dot");
+  var dotsOkay = (slides.length == dots.length);
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    if (dotsOkay) dots[i].style.backgroundColor = "#b0b0b0";
   }
   slideIndex++;
   if (slideIndex == slides.length) {
@@ -42,7 +62,8 @@ function showSlides(Delay) {
     slideIndex = 1;
   }
   slides[slideIndex-1].style.display = "block";
-  if (adjDelay > 0) {
+  if (dotsOkay) dots[slideIndex-1].style.backgroundColor = "black";
+  if ((adjDelay > 0) && (!slidePause)) {
     slideTimer = setTimeout(showSlides, adjDelay);
   } else {
     slideTimer = 0;
