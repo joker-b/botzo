3d6143f969e7945f1c853d83074ddd382fb82e93
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2023-09-22 08:18:13 -0700

    one-step deployment

--- deploy-part1.sh
+++ deploy-part1.sh
@@ -9,7 +9,10 @@ chmod 666 botzo.tar.gz
 scp botzo.tar.gz botzilla.com@botzilla.com:html/botzo.tar.gz
 cd ~/src/botzo
 
-echo Now: 'ssh botzilla.com@botzilla.com'
-echo and: 'cd html; tar -x -f botzo.tar.gz'
+echo "Archive sent, expanding...."
+# echo Now: 'ssh botzilla.com@botzilla.com'
+# echo and: 'cd html; tar -x -f botzo.tar.gz'
+
+ssh botzilla.com@botzilla.com 'cd $HOME/html; tar -x -f botzo.tar.gz; echo A-OKAY'
 
 
